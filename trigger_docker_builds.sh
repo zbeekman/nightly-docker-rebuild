b3c43eba9e85e7d083edce5f9bd7d4eae53fab62
Izaak Beekman <zbeekman@gmail.com>
2017-04-29 10:42:58 -0400

    Reformat with `shfmt -i 2`
     [shfmt on GH]
    
     [shfmt on GH]: https://github.com/mvdan/sh

--- trigger_docker_builds.sh
+++ trigger_docker_builds.sh
@@ -9,10 +9,9 @@ set -o errexit
 # These end point URLs have your secret token in them so make sure to use:
 #    `travis env set DH_END_POINT_1 https://registry.hub.docker.com/u/<USER>/<REPO>/trigger/<TOKEN>/`
 
-for DH_URL_W_TOKEN in ${!DH_END_POINT*} ; do
+for DH_URL_W_TOKEN in ${!DH_END_POINT*}; do
   curl -H "Content-Type: application/json" --data '{"build": true}' -X POST "${!DH_URL_W_TOKEN}"
 done
-
 
 # other examples
 # # Trigger by docker tag name
@@ -21,4 +20,3 @@ done
 # $ curl -H "Content-Type: application/json" --data '{"source_type": "Branch", "source_name": "staging"}' -X POST $DH_URL_W_TOKEN
 # # Trigger by Source tag named v1.1
 # $ curl -H "Content-Type: application/json" --data '{"source_type": "Tag", "source_name": "v1.1"}' -X POST $DH_URL_W_TOKEN
-
